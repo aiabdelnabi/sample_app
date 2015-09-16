4ecd7eb657d4362e75cdfaec00af9af9a459832a
ahmed <ai.abdelnabi@gmail.com>
2015-09-16 20:23:22 +0300

    Add account actvation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 	
+	def setup
+		ActionMailer::Base.deliveries.clear
+	end
+
 	test "invalid signup information" do
 		get signup_path
 		assert_no_difference 'User.count' do
@@ -13,9 +17,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "valid signup information" do
 		get signup_path
 		assert_difference 'User.count', 1 do
-			post_via_redirect users_path, user: { name: "ahmed", email: "ahmed@yahoo.com", password: "123456", password_confirmation: "123456" }
+			post users_path, user: { name: "ahmed", email: "ahmed@yahoo.com", password: "123456", password_confirmation: "123456" }
 		end
-		assert_template 'users/show'
-		assert is_logged_in?
+		assert_equal 1, ActionMailer::Base.deliveries.size
+		user = assigns(:user)
+		assert_not user.activated?
+		# Try to log in before activation
+		log_in_as(user)
+		assert_not is_logged_in?
+		# Invalid activation token
+		get edit_account_activation_path("invalid token")
+		assert_not is_logged_in?
+		get edit_account_activation_path(user.activation_token, email: 'wrong')
+		assert_not is_logged_in?
+		get edit_account_activation_path(user.activation_token, email: user.email)
+		user.reload
+		assert user.activated?
+		follow_redirect!
+		#assert_template 'users/show'
+		#assert is_logged_in?
 	end
 end
