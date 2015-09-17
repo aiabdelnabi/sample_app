d30ddb84fb4c76f6b9047178cd61bffeaaeab785
ahmed <ai.abdelnabi@gmail.com>
2015-09-17 21:29:28 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   # Confirms a logged-in user.
   def logged_in_user
-	unless logged_in?
-		store_location
-		flash[:danger] = "Please login."
-		redirect_to login_url
-	end
+  	unless logged_in?
+  		store_location
+  		flash[:danger] = "Please login."
+  		redirect_to login_url
+  	end
   end
 end
